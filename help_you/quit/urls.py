5b5f6040de50d0adfb81dd11247980ec4d461e7b
Lobashik <lda2005@yandex.ru>
2023-11-30 20:14:49 +0300

    delete commented code and update requirements

--- help_you/quit/urls.py
+++ help_you/quit/urls.py
@@ -5,16 +5,10 @@ from rest_framework.authtoken.views import obtain_auth_token
 from .views import *
 
 
-# app_name = 'registration'
-# add_user_router = DefaultRouter()
-# add_user_router.register('register', RegisterUserView, basename='user-viewset')
-# change_user_router.register('profile', ChangeUserViewSet, basename='profile-viewset')
-
 urlpatterns = [
     path('api/register/', RegisterUserView.as_view()),
     path('api/habits/', BadHabitsListView.as_view()),
     path('api/profile/', ChangeUserView.as_view()),
     path('api/login/', obtain_auth_token)
 ]
-# urlpatterns += [path(r'api/', include(add_user_router.urls))]
-# urlpatterns += [path(r'api', include(change_user_router.urls))]
+
